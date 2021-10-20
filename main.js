13269a8f379e601342c6b5c25b0e95af7cfa4267
Ricky Mormor <rickymormor1@gmail.com>
2021-10-20 14:40:31 +0000

    Create class and methods for book objects

--- main.js
+++ main.js
@@ -5,56 +5,40 @@ const authorInput = document.getElementById('author');
 
 let bookss = [];
 
-const storedBooks = JSON.parse(localStorage.getItem('bookss'));
+class bookClass {
+  constructor(title, author, id) {
+    this.title = title;
+    this.author = author;
+    this.id = id
+  }
 
-function singleBook(book) {
-  return `<li>
-      <p>${book.title}</p>
-      <p>${book.author}</p>
-      <button type="button" data-id='${book.id}' class="remove">remove</button>
-      <hr>
-    </li>`;
-}
+  bookCode() {
+    return `<li>
+    <p>${this.title}</p>
+    <p>${this.author}</p>
+    <button type="button" data-id='${this.id}' class="remove">remove</button>
+    <hr>
+  </li>`;
+  }
 
-function showBooks() {
-  const booksCode = bookss.map((book) => singleBook(book));
-  bookList.innerHTML = booksCode.join('');
-
-  const btn = document.querySelectorAll('.remove');
-
-  btn.forEach((el) => {
-    el.addEventListener('click', (e) => {
-      const id = e.target.getAttribute('data-id');
-      bookss = bookss.filter((b) => b.id !== Number(id));
-      localStorage.setItem('bookss', JSON.stringify(bookss));
-      showBooks();
-    });
-  });
-}
-
-if (storedBooks) {
-  bookss = storedBooks;
-  showBooks();
-}
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const title = titleInput.value.trim();
-  const author = authorInput.value.trim();
-  let id = 1;
+  static addBook(book) {
+    let id = 1;
   if (bookss.length > 0) {
-    id = bookss[bookss.length - 1].id + 1;
+    id = bookss[bookss.length - 1].id + 1; //bookss not dfined yet
   }
-  if (!title || !author) {
-    return;
-  }
-  bookss.push({
-    id,
-    title,
-    author,
-  });
-  showBooks();
+  book.id = id;
+  bookss.push(book);
   localStorage.setItem('bookss', JSON.stringify(bookss));
-  titleInput.value = '';
-  authorInput.value = '';
-});
+}
+
+  static remove(id) {
+    //same as addBook
+    bookss = bookss.filter((b) => b.id !== Number(id));
+    localStorage.setItem('bookss', JSON.stringify(bookss));
+  }
+}
+
+
+
+
+
